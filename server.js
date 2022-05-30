a038648068e71710bebf5fc46e146993632b0343
Anna Liang <yuhan28283@gmail.com>
2022-05-30 16:45:18 -0400

    update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const PORT  = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('../../routes/apiRoutes');
-const htmlRoutes = require('../../routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse(analyze) incoming string or array data
 app.use(express.urlencoded({ extended: true }));
